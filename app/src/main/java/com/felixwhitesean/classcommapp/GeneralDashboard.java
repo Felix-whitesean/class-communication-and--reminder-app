434579f2c9e7cb18bf451ad7e4d73e187cd4880b
felix-whitesean <felixwhitesean@gmail.com>
2024-08-21 06:13:20 +0300

    Fragments and regular amends

--- app/src/main/java/com/felixwhitesean/classcommapp/GeneralDashboard.java
+++ app/src/main/java/com/felixwhitesean/classcommapp/GeneralDashboard.java
@@ -1,64 +1,53 @@
 package com.felixwhitesean.classcommapp;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GeneralDashboard#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class GeneralDashboard extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public GeneralDashboard() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GeneralDashboard.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GeneralDashboard newInstance(String param1, String param2) {
-        GeneralDashboard fragment = new GeneralDashboard();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class GeneralDashboard extends AppCompatActivity {
+    TextView title;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        setContentView(R.layout.general_dashboard);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_general_dashboard, container, false);
+        Button todaysSchedule = findViewById(R.id.todays_schedule);
+        Button upcoming_notification_btn = findViewById(R.id.upcoming_notification_btn);
+        title = findViewById(R.id.title);
+
+        todaysSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                title.setText("Today's Schedule");
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new TodaysSchedule())
+                        .commit();
+            }
+        });
+        upcoming_notification_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                title.setText("Upcoming Notification");
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new UpcomingNotifications())
+                        .commit();
+            }
+        });
+        if(savedInstanceState == null) {
+            title.setText("Today's Schedule");
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new TodaysSchedule())
+                    .commit();
+        }
     }
 }
