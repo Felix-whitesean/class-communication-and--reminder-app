2226ab88209d1538c37d7060eded4514e629f583
shidycool <user.rashfrdd@gmail.com>
2024-08-22 14:53:26 +0300

    alarm and notification.

--- app/src/main/java/com/felixwhitesean/classcommapp/MainActivity.java
+++ app/src/main/java/com/felixwhitesean/classcommapp/MainActivity.java
@@ -5,7 +5,9 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +19,7 @@ import com.google.firebase.FirebaseApp;
 public class MainActivity extends AppCompatActivity {
     private static final String PREFS_NAME = "UserLoginPrefs";
     private static final String IS_LOGGED_IN = "IsLoggedIn";
+    private AlarmManager alarmManager;
 
 
     @Override
@@ -36,15 +39,14 @@ public class MainActivity extends AppCompatActivity {
         getStartedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                setAlarm();
-                Log.d("acknowledgment","Getting started button clicked");
-//                // Navigate to authentication_page_activity
+                setAlarm();
+                Log.d("acknowledgment", "Getting started button clicked");
+                // Navigate to authentication_page_activity
                 if (isLoggedIn) {
                     // User is logged in, redirect to ScheduleSettings
                     Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                     startActivity(intent);
-                }
-                else {
+                } else {
                     Toast.makeText(MainActivity.this, "Sign up to continue", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
                     startActivity(intent);
@@ -54,20 +56,31 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-//    private void setAlarm() {
-//        // Set the alarm to start 30 seconds from now
-//        Calendar calendar = Calendar.getInstance();
-//        calendar.add(Calendar.SECOND, 30); // Set the alarm to trigger after 30 seconds
-//
-//        // Create an intent to the BroadcastReceiver
-//        Intent intent = new Intent(this, AlarmReceiver.class);
-//        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
-//
-//        // Set the alarm
-//        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-//        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-//
-//
-//        Log.d("Alarm", "Alarm set for 30 seconds from now.");
-//    }
+
+    private void setAlarm() {
+        // Set the alarm to start 30 seconds from now
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.SECOND, 30); // Set the alarm to trigger after 30 seconds
+
+        // Create an intent to the BroadcastReceiver
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        // Get the AlarmManager service
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        // Set the exact alarm
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (!alarmManager.canScheduleExactAlarms()) {
+                intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+                startActivity(intent);
+            } else {
+                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+            }
+        } else {
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        }
+
+        Log.d("Alarm", "Alarm set for 30 seconds from now.");
+    }
 }
